62a006cb8ece1ca0dec7931e56366baeb187f6ff
Yann Viens <viensyann@gmail.com>
2017-06-04 18:20:06 +0100

    Move MainActvity to a new package.

--- app/src/main/java/com/yannshu/epicpicturesofearth/ui/activities/MainActivity.kt
+++ app/src/main/java/com/yannshu/epicpicturesofearth/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yannshu.epicpicturesofearth
+package com.yannshu.epicpicturesofearth.ui.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.yannshu.epicpicturesofearth.R
 
 class MainActivity : AppCompatActivity() {
 
