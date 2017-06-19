bcdc81f3c0aca435398e3c7634597d0e5e85d0eb
Yann Viens <viensyann@gmail.com>
2017-06-19 18:29:50 +0100

    Add annotation to correctly inject activityComponentBuilders.

--- app/src/main/java/com/yannshu/epicpicturesofearth/EpicPicturesOfEarthApplication.kt
+++ app/src/main/java/com/yannshu/epicpicturesofearth/EpicPicturesOfEarthApplication.kt
@@ -6,12 +6,13 @@ import com.yannshu.epicpicturesofearth.di.activity.ActivityComponentBuilder
 import com.yannshu.epicpicturesofearth.di.activity.HasActivitySubComponentBuilders
 import com.yannshu.epicpicturesofearth.di.app.AppComponent
 import com.yannshu.epicpicturesofearth.di.app.AppModule
+import com.yannshu.epicpicturesofearth.di.app.DaggerAppComponent
 import javax.inject.Inject
 import javax.inject.Provider
 
 class EpicPicturesOfEarthApplication : Application(), HasActivitySubComponentBuilders {
     @Inject
-    lateinit var mActivityComponentBuilders: Map<Class<out Activity>, Provider<ActivityComponentBuilder<*, *>>>
+    lateinit var mActivityComponentBuilders: Map<Class<out Activity>, @JvmSuppressWildcards Provider<ActivityComponentBuilder<*, *>>>
 
     lateinit var mAppComponent: AppComponent
 
