6e53037b4dfd63143c052bdea69145035b83253c
Yann Viens <viensyann@gmail.com>
2017-07-18 21:16:33 +0100

    Setup CalendarView logic.

--- app/src/main/java/com/yannshu/epicpicturesofearth/view/activities/HomeActivity.kt
+++ app/src/main/java/com/yannshu/epicpicturesofearth/view/activities/HomeActivity.kt
@@ -2,17 +2,25 @@ package com.yannshu.epicpicturesofearth.view.activities
 
 import android.arch.lifecycle.Observer
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.support.v4.view.ViewCompat
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import butterknife.BindView
 import butterknife.ButterKnife
+import butterknife.OnClick
+import com.github.sundeepk.compactcalendarview.CompactCalendarView
 import com.yannshu.epicpicturesofearth.R
 import com.yannshu.epicpicturesofearth.data.model.PictureMetadata
 import com.yannshu.epicpicturesofearth.di.activity.HasActivitySubComponentBuilders
 import com.yannshu.epicpicturesofearth.view.adapters.PicturesAdapter
 import com.yannshu.epicpicturesofearth.view.base.BaseActivity
 import com.yannshu.epicpicturesofearth.view.model.PicturesMetadataListViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 
@@ -30,8 +38,14 @@ class HomeActivity : BaseActivity() {
     @Inject
     lateinit var mItemDecoration: RecyclerView.ItemDecoration
 
+    @Inject
+    lateinit var mDateFormat: SimpleDateFormat
+
     @BindView(R.id.pictures_recycler_view)
     lateinit var mRecyclerView: RecyclerView
+
+    @BindView(R.id.app_bar_layout)
+    lateinit var mAppBarLayout: AppBarLayout
 
     @BindView(R.id.title_text_view)
     lateinit var mTitleTextView: TextView
@@ -39,17 +53,23 @@ class HomeActivity : BaseActivity() {
     @BindView(R.id.subtitle_text_view)
     lateinit var mSubtitleTextView: TextView
 
+    @BindView(R.id.calendar_view)
+    lateinit var mCalendarView: CompactCalendarView
+
+    @BindView(R.id.date_picker_arrow)
+    lateinit var mArrowImageView: ImageView
+
     var mQuality: String = "natural"
 
-    var mDate: String = "2017-06-20"
+    var mIsExpanded: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         ButterKnife.bind(this)
         initLayout()
+        setCurrentDate(Date())
 
-        mViewModel.init(mQuality, mDate)
         mViewModel.mPicturesMetadata?.observe(this, object : Observer<List<PictureMetadata>> {
             override fun onChanged(picturesMetadata: List<PictureMetadata>?) {
                 Log.d("data", "pictures: " + picturesMetadata?.size)
@@ -72,8 +92,8 @@ class HomeActivity : BaseActivity() {
     private fun initLayout() {
         initRecyclerView()
         initActionBar(false)
+        initCalendar();
         setTitle(getString(R.string.app_name))
-        setSubtitle(mDate)
     }
 
     private fun initRecyclerView() {
@@ -89,11 +109,44 @@ class HomeActivity : BaseActivity() {
         }
     }
 
+    private fun initCalendar() {
+        mCalendarView.setLocale(TimeZone.getDefault(), Locale.getDefault())
+        mCalendarView.setShouldDrawDaysHeader(true)
+        mCalendarView.setListener(object : CompactCalendarView.CompactCalendarViewListener {
+            override fun onDayClick(dateClicked: Date) {
+                setCurrentDate(dateClicked)
+            }
+
+            override fun onMonthScroll(firstDayOfNewMonth: Date) {
+            }
+        })
+    }
+
     private fun setTitle(title: String) {
         mTitleTextView.text = title
     }
 
     private fun setSubtitle(subtitle: String) {
         mSubtitleTextView.text = subtitle
+    }
+
+    private fun setCurrentDate(date: Date) {
+        val formattedDate = mDateFormat.format(date)
+        if (formattedDate != null) {
+            setSubtitle(formattedDate)
+            mCalendarView.setCurrentDate(date)
+            mViewModel.init(mQuality, formattedDate)
+        }
+    }
+
+    @OnClick(R.id.date_picker_layout)
+    fun onDatePickerClick(view: View) {
+        if (mIsExpanded) {
+            ViewCompat.animate(mArrowImageView).rotation(0.0f).start();
+        } else {
+            ViewCompat.animate(mArrowImageView).rotation(180.0f).start();
+        }
+        mIsExpanded = !mIsExpanded
+        mAppBarLayout.setExpanded(mIsExpanded, true)
     }
 }
